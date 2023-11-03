858439dd3e79ee00c39616eeb308b226f715a539
Silivonchik Swetlana <silivonchik.svetlana@gmx.de>
2023-11-03 06:44:12 +0100

    make addition calculator

--- src/main/java/Src/HWorld.java
+++ src/main/java/Src/HWorld.java
@@ -1,7 +1,12 @@
 package Src;
 
+import java.util.Scanner;
+
 public class HWorld {
     public static void main(String[] args) {
-        System.out.println("Hallö W");
+        Scanner scanner=new Scanner(System.in);
+        int a= scanner.nextInt();
+        int b=scanner.nextInt();
+        System.out.println(a+b);
     }
 }
