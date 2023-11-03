6060f30836d06789fb52146c95c8bd63c4e46cc2
Silivonchik Swetlana <silivonchik.svetlana@gmx.de>
2023-11-03 20:30:25 +0100

    Main.java

---
+++ src/main/java/Main02/Main.java
@@ -0,0 +1,9 @@
+package Main02;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This issample task");
+        System.out.println("Feature test");
+
+    }
+}
